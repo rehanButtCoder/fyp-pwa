bb6eca673157d5ddc0b0f03cf7f550fb70962f65
Butt, Rehan Butt <rehan.butt15555@gmail.com>
2024-01-11 03:11:16 +0500

    logo added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
-export default function Home() {
+import Image from "next/image";
 
+export default function Home() {
   return (
     <div>
+      <Image
+        src="/images/profile.png"
+        width={111}
+        height={111}
+        alt="Picture of the author"
+      />
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
